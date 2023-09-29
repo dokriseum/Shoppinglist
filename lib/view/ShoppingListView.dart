083e2663f0155d68d15ef3ac301b05c6de2a37f0
dokriseum <eiki300499@gmail.com>
2023-09-29 23:40:17 +0200

    Popup Item hinzufügen

--- lib/view/ShoppingListView.dart
+++ lib/view/ShoppingListView.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../model/ShoppingItem.dart';
-
-
 class ShoppingListView extends StatefulWidget {
   @override
   _ShoppingListViewState createState() => _ShoppingListViewState();
@@ -11,7 +8,8 @@ class ShoppingListView extends StatefulWidget {
 class _ShoppingListViewState extends State<ShoppingListView> {
   List<ShoppingItem> items = [];
   String newItemName = '';
-  int newItemQuantity = 1;
+  String newItemQuantity = '1'; // Anzahl als Zeichenkette, um die Eingabe zu verarbeiten
+  final TextEditingController _quantityController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -35,23 +33,67 @@ class _ShoppingListViewState extends State<ShoppingListView> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _addItem();
+          _showNewItemDialog();
         },
         child: Icon(Icons.add),
       ),
     );
   }
 
-  void _addItem() {
-    setState(() {
-      var newItem = ShoppingItem(name: newItemName, quantity: newItemQuantity);
-      items.add(newItem);
-      // Hier kannst du die Synchronisierung mit anderen Geräten implementieren
-      // Zum Beispiel über eine Datenbank oder Cloud-Speicher
-      newItemName = '';
-      newItemQuantity = 1;
-    });
+  void _showNewItemDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Neues Produkt hinzufügen'),
+          content: Column(
+            children: <Widget>[
+              TextField(
+                onChanged: (value) {
+                  newItemName = value;
+                },
+                decoration: InputDecoration(labelText: 'Produkt'),
+              ),
+              TextField(
+                controller: _quantityController,
+                onChanged: (value) {
+                  newItemQuantity = value;
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Anzahl'),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Abbrechen'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Hinzufügen'),
+              onPressed: () {
+                int quantity = int.tryParse(newItemQuantity) ?? 1; // Konvertiere zu einer Zahl oder verwende 1 als Standardwert
+                ShoppingItem newItem = ShoppingItem(name: newItemName, quantity: quantity);
+                setState(() {
+                  items.add(newItem);
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
 
-void main() => runApp(MaterialApp(home: ShoppingListView()));
+class ShoppingItem {
+  String name;
+  int quantity;
+
+  ShoppingItem({required this.name, required this.quantity});
+}
+
+//void main() => runApp(MaterialApp(home: ShoppingListView()));
